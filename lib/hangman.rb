5f9a72da05e76f7d869e841169bbf870164510a7
Pemdas1991 <Pemdas1991@gmail.com>
2023-03-10 16:15:00 -0700

    Add player_guess and validation method

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,60 +1,98 @@
 require_relative 'draw.rb'
+require 'pry'
 
+
+def clear
+  system('clear')
+end
+
+def select_game_type
+
+end
 
 def pick_word
   word = ""
   word_list = File.readlines('google-10000-english-no-swears.txt')
   seed = Random.new
-  while word.length <=5 || word.length > 11
-    word = word_list[seed.rand(word_list.length)]
-    word
+  while word.length <=5 || word.length > 11 do
+    word = word_list[seed.rand(word_list.length)].chomp
   end
+  word
 end
 
 
 def new_game
-  word = pick_word()
+  @game_word = pick_word.upcase
+  p @game_word
+  @failures = 0
+  @hidden_word = Array.new(@game_word.length,'_')
+  p @hidden_word
 end
 
-def load_game
+def get_player_guess
+  
+  guess_valid = false
 
+    while !guess_valid do
+    puts "Enter your guess"
+    guess = gets.chomp.upcase
+    
+      if guess.length != 1
+        puts "Invalid Guess"
+      else  
+        guess_valid = true
+      end
+    guess
+  end
 end
+
+
+def compare_guess(letter)
+  
+end
+
+def valid_letter?(letter)
+  puts letter
+end
+  
 
 def hangman
 
-  in_progress = false
-  failures = 0
-  guesses = 0
-  guessed_letter = Array.new
-  game_word = ""
-  hidden_word = Array.new
+  @failures = 0
+  @guesses = 0
+  @game_word = ""
+  @hidden_word = {}
+  @guessed_letters = Array.new
+  
 
+  in_progress = false
+  
   p 'Welcome to hang man!'
 
-  while !in_progress
+  while !in_progress do
 
     p 'Type 1 to start a new game,'
     p 'Type 2 to load a saved game,'
     game_type = gets.chomp
 
     if game_type.to_i == 1
+      #clear
       in_progress = true
       new_game
     elsif game_type.to_i == 2
+      clear
       puts 'Loading not yet implemented try again'
     else
-      system('clear')
+      clear
      puts 'Try Again'
     end
   end
-
-  
-   puts game_type
   
   
   while in_progress
+    valid_letter?(get_player_guess)
 
-    draw[failures]
+    in_progress = false
 
   end
   #variables
@@ -85,4 +123,12 @@ def hangman
 
 end
 
+
 hangman
+
+
+
+
+def load_game
+
+end
