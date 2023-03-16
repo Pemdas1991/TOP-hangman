fbfa5e870fb9051a0ff323c11646c32ee588775a
Pemdas1991 <Pemdas1991@gmail.com>
2023-03-16 07:31:59 -0600

    update formating with rubocop

--- lib/draw.rb
+++ lib/draw.rb
@@ -1,62 +1,62 @@
+frozen_string_literal: true
+
 def draw(failures)
   clear
   hang_array = ["
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
-    |    
-    |   
-    |   
-    | 
+    |
+    |
+    |
+    |
     ========",
-    "
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
-    |   
-    |   
-    | 
+    |
+    |
+    |
     ========",
-    "
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
     |    |
-    |   
-    | 
+    |
+    |
     ========",
-    "
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
     |   /|
-    |   
-    | 
+    |
+    |
     ========",
-    "
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
     |   /|\\
-    |   
-    | 
+    |
+    |
     ========",
-    "
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
     |   /|\\
-    |   / 
-    | 
-    ========",  
-    "
+    |   /
+    |
+    ========",
+                "
     +----+     Guesses: #{@guesses}
     |    |     #{@incorrect_letters}
     |    0
     |   /|\\
     |   / \\
-    | 
+    |
     ========"]
-    puts hang_array[failures]
+  puts hang_array[failures]
 end
-
-
